d4be07fc32b8dc7a9bd958a35eecb6c97ec3f383
MatiKodzi <mrudyk96@gmail.com>
2021-12-07 16:30:40 +0100

    layout done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+
+import React, { Component } from 'react'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const Messages = () => {
+		return (
+			<div className="messages">
+
+			</div>
+		)
+	};
+
+	const Members = () => {
+		return (
+			<div className="members">
+
+			</div>
+		)
+	};
+
+	const MessageForm = () => {
+		return (
+			<div>
+				<form className="mess-form">
+					<button className="mess-btn"><i class="far fa-paper-plane fa-2x"></i></button>
+					<textarea name="option" className="mess-input" />
+					<div className="mess-info"></div>
+				</form >
+			</div >
+		)
+	};
+
+
+	return (
+		<div className="App">
+			<Messages />
+			<Members />
+			<MessageForm />
+		</div>
+	);
 }
 
+
+
 export default App;
+
+
+
+
+
+// <header className="App-header">
+// 				<img src={logo} className="App-logo" alt="logo" />
+// 			</header>
